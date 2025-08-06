45ac22fc1a9832a8aaea74fd4767fedefd83faf4
claudeayi <claudejordanayissi@gmail.com>
2025-08-06 14:52:13 +0100

    Create CRM index.ts

---
+++ CRM index.ts
@@ -0,0 +1,11 @@
+import ClientsView from './views/ClientsView/ClientsView';
+import DealsView from './views/DealsView/DealsView';
+import ActivitiesView from './views/ActivitiesView/ActivitiesView';
+import ReportsView from './views/ReportsView/ReportsView';
+
+export { 
+  ClientsView, 
+  DealsView, 
+  ActivitiesView, 
+  ReportsView 
+};
