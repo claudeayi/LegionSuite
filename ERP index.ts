85263ea0c67bebd113ddddcd7f0945accc4f7bd7
claudeayi <claudejordanayissi@gmail.com>
2025-08-06 15:09:26 +0100

    Create ERP index.ts

---
+++ ERP index.ts
@@ -0,0 +1,15 @@
+import ProductsView from './views/ProductsView/ProductsView';
+import InventoryDashboard from './views/InventoryView/InventoryDashboard';
+import OrdersView from './views/OrdersView/OrdersView';
+import ProductionDashboard from './views/ProductionView/ProductionDashboard';
+import AccountingDashboard from './views/AccountingView/AccountingDashboard';
+import HRDashboard from './views/HRView/HRDashboard';
+
+export { 
+  ProductsView, 
+  InventoryDashboard, 
+  OrdersView, 
+  ProductionDashboard,
+  AccountingDashboard,
+  HRDashboard
+};
